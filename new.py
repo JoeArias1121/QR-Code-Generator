e1799c1a1934a5b99fa2385fb7657ce83d49cfd3
joearias1121 <joearias1121@gmail.com>
2025-01-03 20:01:06 -0500

    Initial commit

---
+++ new.py
@@ -0,0 +1,5 @@
+from PIL import Image
+
+img = Image.new('1', (100, 100), 0)
+
+img.show()
---
+++ open.py
@@ -0,0 +1,7 @@
+from PIL import Image
+
+filename = 'resources/penguin.jpg'
+
+img = Image.open(filename)
+
+img.show()
