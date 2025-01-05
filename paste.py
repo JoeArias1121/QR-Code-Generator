0749f5d4707da51210695bf9eab53da93700836e
joearias1121 <joearias1121@gmail.com>
2025-01-05 14:01:49 -0500

    made code for top left postion pattern (brute force)

--- paste.py
+++ paste.py
@@ -13,12 +13,22 @@ pmax = 352
 rmax = 8
 rpmax = 0
 
+postion_pattern = Image.new('1', (64, 64), 1)
+position_y = Image.new('1', (56, 8), 0)
+position_x = Image.new('1', (8, 40), 0)
+innerbox = Image.new('1', (24, 24), 0)
+postion_pattern.paste(position_y, (0, 0, 56, 8))
+postion_pattern.paste(position_y, (0, 48, 56, 56))
+postion_pattern.paste(position_x, (0, 8, 8, 48))
+postion_pattern.paste(position_x, (48, 8, 56, 48))
+postion_pattern.paste(innerbox, (16, 16, 40, 40))
 
+'''
 while rmax <= maxx and rpmax <= pmax:
     img.paste(bit, (rpmax, rpmax, rmax, rmax))
     rmax += 8
     rpmax += 8
+'''
 
-
-
-img.show()
+postion_pattern.show()
+#img.show()
